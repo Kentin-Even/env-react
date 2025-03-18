62d5568f3d9ef39bfe827a7376a981b20ead8302
Kentin Even <kentineven02@gmail.com>
2025-03-18 09:24:31 +0100

    chore: add .env and commitizen config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.js"],
   testMatch: ["**/__tests__/**/*.test.js", "**/*.test.js"],
-  collectCoverage: false,
+  collectCoverage: true,
   coverageReporters: ["text"],
   collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/tests/", "!/node_modules/**"]
 }
